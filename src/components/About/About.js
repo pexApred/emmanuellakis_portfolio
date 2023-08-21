755a66c01a7258ed17a7d7f45fd9da394c6f4fad
pexApred <120075705+pexApred@users.noreply.github.com>
2023-08-21 15:56:08 -0400

    Edited About me section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,24 +11,22 @@ function About() {
                 <Accordion.Item eventKey="0">
                     <Accordion.Header className="header-about text-center">About Me</Accordion.Header>
                     <Accordion.Body className="body-about">
-                        Dynamic Full-Stack Web Developer and experienced business owner
-                        restaurant. Recently transitioned into tech, completing a
-                        rigorous Full-Stack Coding Bootcamp from the University of
-                        North Carolina at Chapel Hill. Proficient in a range of modern
-                        technologies including HTML/CSS, JavaScript, Node.js, Express.js,
-                        MySQL, MongoDB, and React.
-                        My background in the fast-paced restaurant industry has honed my
-                        problem-solving abilities, adaptability, and leadership skills.
-                        I bring these strengths to my work in web development, alongside
-                        a strong technical skill set. In my coding bootcamp, I successfully
-                        completed several full-stack applications and collaborated on team
-                        projects using Agile methodologies.
-                        I'm now looking to leverage my unique blend of skills in a
-                        challenging full-stack web developer role. I bring with me not just
-                        coding skills, but also a track record of strategic thinking,
-                        team management, and the ability to thrive under pressure. My goal
-                        is to create efficient and user-friendly web applications that meet
-                        the needs of users and exceed client expectations.
+                        <p>
+                            Yia sas! I'm Emmanuel, a Full-Stack Web Developer with an entrepreneurial spirit honed through years of leading a thriving restaurant.
+                            Hospitality taught me that every second counts, that proper strategies can make or break the night, and that adaptability is the secret ingredient to success.
+                        </p>
+                        <p>
+                            Swapping the aroma of freshly baked pizza for the world of code, I immersed myself in a rigorous Full-Stack Coding Bootcamp at the University of North Carolina at Chapel Hill.
+                            I now harness a range of modern technologies including HTML/CSS, JavaScript, Node.js, Express.js,MySQL, MongoDB, and React.
+                            I see code as my new kitchen: a place to experiment, to create, and to grow.
+                        </p>
+                        <p>
+                            My years in the restaurant business equipped me with problem-solving skills, an unwavering ability to lead, and the tenacity to thrive under pressure. 
+                            I bring these strengths to my work in web development, alongside a strong technical skill set. During my bootcamp, I engineered several full-stack applications and collaborated on team projects using Agile methodologies.
+                        </p>
+                        <p>
+                            Now, I'm on the lookout for my next adventure: a challenging full-stack developer role where I can mesh my rich past with my vibrant tech future. I aspire to keep coding, keep learning, and to design applications that are as delightful to users as a slice of the finest Margherita.
+                        </p>
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
