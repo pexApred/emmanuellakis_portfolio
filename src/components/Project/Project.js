0db2727d39b3c72c2f2747428f6f0aa1bfafa29b
pexApred <120075705+pexApred@users.noreply.github.com>
2023-11-20 20:18:42 -0500

    Spell checked.

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -5,9 +5,15 @@ import './Project.css';
 const projects = [
     {
         title: 'Wonderful World of dR. Slide: Riddle Book',
-        description: '',
+        description: `Welcome to The Wonderful World of Dr. Slide! 
+        To get started, please create a username and password in order to access the site. 
+        From there, you will be able to solve the riddles and see how well you can do overall. Need help? You can ask for a hint and then try again. 
+        Alas, some are quite difficult so if the answer still evades you then click "I Give Up" to see the solution. 
+        It is highly encouraged that you try for a while before giving up as some require quite a lot of thought before the answer may become clearer. On this site, you will be also able to see your own personal statistics with regards to your success rate on the riddles. 
+        Are you amongst the best of the best? Your username will proudly be displayed on the Leader Board for all to see. 
+        Regardless of your success, please enjoy the process and confort in knowing you are exercising your brain.`,
         image: '../wwofdrslide-screenshot.png',
-        link: ''
+        link: 'https://github.com/pexApred/WWofDrSlide.git'
     },
     {
         title: 'JourneyVerse: Interactive MERN Stack',
